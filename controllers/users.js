f91c6a555fe7b581feaae4963ab2a544adcf8857
AnnTin <ann.antin@yandex.ru>
2023-07-31 13:49:29 +0300

    Pr 13 - 2nd try

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,4 @@
-const User = require('../models/user');
+const User = require('../models/users');
 const NotFound = require('../errors/errorCodes');
 const STATUS_CODE = require('../errors/errorCodes');
 
